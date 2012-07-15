659355b9c523a38ea94c0d05b3b66fc79caa5aae
Ben Whittle <benjiwhittle@gmail.com>
2012-07-15 17:22:51 +0100

    Added IMath service

---
+++ ServiceLibrary/Maths.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Maths" in both code and config file together.
+    public class Maths : IMaths
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
